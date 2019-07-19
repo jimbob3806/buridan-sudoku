b8417f40adf1d7f56af852e923f42c9419e853fc
jimbob3806 <jamesreid3806@gmail.com>
2019-07-19 15:35:32 +0100

    Updates

---
+++ README.md
@@ -0,0 +1,41 @@
+# Notes on ./public usage
+
+## manifest.json
+The **theme_color**  value dictates the color of the toolbar in a PWA, and the 
+color of the url bar (or whatever you want to call it!) in an appropriate
+browser.
+
+The **background_color** value actually dictates the color of the splashscreen
+or loading screen in a PWA. Hence why it can be non-white without looking 
+stupid!
+
+## robots.txt
+Parts of site disallowed (in order they appear in the robots.txt file):
+* Authenticated users pages (except blog, which conditionally renders
+premium and free content depending on subscription status)
+* Callback page(s)
+* Error pages, such as 400 /badRequest etc.
+
+#### robot.txt guide of available values:
+* User-agent: The specific web crawler to which you’re giving crawl 
+instructions (usually a search engine). A list of most user agents can be 
+found here.
+* Disallow: The command used to tell a user-agent not to crawl particular 
+URL. Only one "Disallow:" line is allowed for each URL.
+* Allow (Only applicable for Googlebot): The command to tell Googlebot it 
+can access a page or subfolder even though its parent page or subfolder may 
+be disallowed.
+* Crawl-delay: How many seconds a crawler should wait before loading and 
+crawling page content. Note that Googlebot does not acknowledge this 
+command, but crawl rate can be set in Google Search Console (just google
+"google search console", and navigate to your account to find out more).
+* Sitemap: Used to call out the location of any XML sitemap(s) associated 
+with this URL. Note this command is only supported by Google, Ask, Bing, 
+and Yahoo.
+
+#### robots.txt pattern matching
+ * \* character is a wildcard that represents any sequence of characters
+ * $ character matches the end of the URL
+
+Find out more here (https://support.google.com/webmasters/answer/6062596?hl=en) 
+and here (https://moz.com/learn/seo/robotstxt)
---
+++ precache-manifest.f42440055432a4d2e2b43d24a883f07a.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3c97b9021d671a3013a6b80aa6cf1fc",
+    "url": "/buridan-sudoku/index.html"
+  },
+  {
+    "revision": "a7da455a5cb2ed0ba9c1",
+    "url": "/buridan-sudoku/static/css/2.48a41c52.chunk.css"
+  },
+  {
+    "revision": "4733a1b05d36fde71627",
+    "url": "/buridan-sudoku/static/css/main.5ef8425e.chunk.css"
+  },
+  {
+    "revision": "a7da455a5cb2ed0ba9c1",
+    "url": "/buridan-sudoku/static/js/2.af434bba.chunk.js"
+  },
+  {
+    "revision": "4733a1b05d36fde71627",
+    "url": "/buridan-sudoku/static/js/main.87fa6f01.chunk.js"
+  },
+  {
+    "revision": "cc70303cc9e7d1bbee34",
+    "url": "/buridan-sudoku/static/js/runtime~main.1943f373.js"
+  },
+  {
+    "revision": "987b84570ea69ee660455b8d5e91f5f1",
+    "url": "/buridan-sudoku/static/media/roboto-latin-100.987b8457.woff2"
+  },
+  {
+    "revision": "e9dbbe8a693dd275c16d32feb101f1c1",
+    "url": "/buridan-sudoku/static/media/roboto-latin-100.e9dbbe8a.woff"
+  },
+  {
+    "revision": "6232f43d15b0e7a0bf0fe82e295bdd06",
+    "url": "/buridan-sudoku/static/media/roboto-latin-100italic.6232f43d.woff2"
+  },
+  {
+    "revision": "d704bb3d579b7d5e40880c75705c8a71",
+    "url": "/buridan-sudoku/static/media/roboto-latin-100italic.d704bb3d.woff"
+  },
+  {
+    "revision": "55536c8e9e9a532651e3cf374f290ea3",
+    "url": "/buridan-sudoku/static/media/roboto-latin-300.55536c8e.woff2"
+  },
+  {
+    "revision": "a1471d1d6431c893582a5f6a250db3f9",
+    "url": "/buridan-sudoku/static/media/roboto-latin-300.a1471d1d.woff"
+  },
+  {
+    "revision": "210a7c781f5a354a0e4985656ab456d9",
+    "url": "/buridan-sudoku/static/media/roboto-latin-300italic.210a7c78.woff"
+  },
+  {
+    "revision": "d69924b98acd849cdeba9fbff3f88ea6",
+    "url": "/buridan-sudoku/static/media/roboto-latin-300italic.d69924b9.woff2"
+  },
+  {
+    "revision": "5d4aeb4e5f5ef754e307d7ffaef688bd",
+    "url": "/buridan-sudoku/static/media/roboto-latin-400.5d4aeb4e.woff2"
+  },
+  {
+    "revision": "bafb105baeb22d965c70fe52ba6b49d9",
+    "url": "/buridan-sudoku/static/media/roboto-latin-400.bafb105b.woff"
+  },
+  {
+    "revision": "9680d5a0c32d2fd084e07bbc4c8b2923",
+    "url": "/buridan-sudoku/static/media/roboto-latin-400italic.9680d5a0.woff"
+  },
+  {
+    "revision": "d8bcbe724fd6f4ba44d0ee6a2675890f",
+    "url": "/buridan-sudoku/static/media/roboto-latin-400italic.d8bcbe72.woff2"
+  },
+  {
+    "revision": "285467176f7fe6bb6a9c6873b3dad2cc",
+    "url": "/buridan-sudoku/static/media/roboto-latin-500.28546717.woff2"
+  },
+  {
+    "revision": "de8b7431b74642e830af4d4f4b513ec9",
+    "url": "/buridan-sudoku/static/media/roboto-latin-500.de8b7431.woff"
+  },
+  {
+    "revision": "510dec37fa69fba39593e01a469ee018",
+    "url": "/buridan-sudoku/static/media/roboto-latin-500italic.510dec37.woff2"
+  },
+  {
+    "revision": "ffcc050b2d92d4b14a4fcb527ee0bcc8",
+    "url": "/buridan-sudoku/static/media/roboto-latin-500italic.ffcc050b.woff"
+  },
+  {
+    "revision": "037d830416495def72b7881024c14b7b",
+    "url": "/buridan-sudoku/static/media/roboto-latin-700.037d8304.woff2"
+  },
+  {
+    "revision": "cf6613d1adf490972c557a8e318e0868",
+    "url": "/buridan-sudoku/static/media/roboto-latin-700.cf6613d1.woff"
+  },
+  {
+    "revision": "010c1aeee3c6d1cbb1d5761d80353823",
+    "url": "/buridan-sudoku/static/media/roboto-latin-700italic.010c1aee.woff2"
+  },
+  {
+    "revision": "846d1890aee87fde5d8ced8eba360c3a",
+    "url": "/buridan-sudoku/static/media/roboto-latin-700italic.846d1890.woff"
+  },
+  {
+    "revision": "19b7a0adfdd4f808b53af7e2ce2ad4e5",
+    "url": "/buridan-sudoku/static/media/roboto-latin-900.19b7a0ad.woff2"
+  },
+  {
+    "revision": "8c2ade503b34e31430d6c98aa29a52a3",
+    "url": "/buridan-sudoku/static/media/roboto-latin-900.8c2ade50.woff"
+  },
+  {
+    "revision": "7b770d6c53423deb1a8e49d3c9175184",
+    "url": "/buridan-sudoku/static/media/roboto-latin-900italic.7b770d6c.woff2"
+  },
+  {
+    "revision": "bc833e725c137257c2c42a789845d82f",
+    "url": "/buridan-sudoku/static/media/roboto-latin-900italic.bc833e72.woff"
+  }
+]);
